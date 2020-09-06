887a0b99582db8f994ff33d166963d9e0e585c09
Valentin Mamontov <mamontov.vs@yandex.ru>
2020-09-06 10:54:43 +0300

    Updates

---
+++ precache-manifest.f8bfdc546b6f9380c7156baaa3653ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a40ac314bde1ad99d376daf358b4db",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "30c29dce87eb349bfca7",
+    "url": "/robots/static/css/main.17c3dd53.chunk.css"
+  },
+  {
+    "revision": "6032842d938ecf18a14f",
+    "url": "/robots/static/js/2.5f4f605c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.5f4f605c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c29dce87eb349bfca7",
+    "url": "/robots/static/js/main.95c6abb6.chunk.js"
+  },
+  {
+    "revision": "3fb9ada94638ecd9bd38",
+    "url": "/robots/static/js/runtime-main.9d0b99cd.js"
+  }
+]);
